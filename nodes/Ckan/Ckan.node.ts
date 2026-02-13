ac93443611d49c824174c60eeba1b8e1e9486e4b
Juli0an693 <206093480+Juli0an693@users.noreply.github.com>
2026-02-13 13:48:12 +0100

    Update Icon

--- nodes/Ckan/Ckan.node.ts
+++ nodes/Ckan/Ckan.node.ts
@@ -13,7 +13,7 @@ export class Ckan implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'CKAN',
 		name: 'ckan',
-		icon: { light: 'file:ckan.png', dark: 'file:ckan.png' },
+		icon: { light: 'file:ckan.svg', dark: 'file:ckan.dark.svg' },
 		group: ['transform'],
 		version: 1,
 		subtitle: '={{$parameter["operation"]}}',
